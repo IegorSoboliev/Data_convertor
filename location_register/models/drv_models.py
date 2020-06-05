b2221aada8d3b5e08abce9d70dbcfd7b22bbada0
IegorSoboliev <evsobolev@gmail.com>
2020-06-05 16:09:33 +0300

    Parser and service for DrvAtos, DrvStreets and DrvBuildings

--- location_register/models/drv_models.py
+++ location_register/models/drv_models.py
@@ -49,9 +49,10 @@ class DrvStreet(DataOceanModel):
     district = models.ForeignKey(DrvDistrict, on_delete=models.CASCADE)
     council = models.ForeignKey(DrvCouncil, on_delete=models.CASCADE)
     ato = models.ForeignKey(DrvAto, on_delete=models.CASCADE)
+    code = models.CharField(max_length=15, unique=True)
     name = models.CharField(max_length=100)
-    previous_name = models.CharField(max_length=100)
-    number_of_buildings = models.IntegerField()
+    previous_name = models.CharField(max_length=100, null=True)
+    number_of_buildings = models.PositiveIntegerField(null=True)
     
     def __str__(self):
         return self.name
@@ -62,20 +63,21 @@ class ZipCode(DataOceanModel):
     district = models.ForeignKey(DrvDistrict, on_delete=models.CASCADE)
     council = models.ForeignKey(DrvCouncil, on_delete=models.CASCADE)
     ato = models.ForeignKey(DrvAto, on_delete=models.CASCADE)
-    code = models.IntegerField()
+    code = models.CharField(max_length=6, unique=True)
 
     def __str__(self):
         return self.code
 
 
 class DrvBuilding(DataOceanModel):
+    INVALID = 'INVALID'
     region = models.ForeignKey(DrvRegion, on_delete=models.CASCADE)
     district = models.ForeignKey(DrvDistrict, on_delete=models.CASCADE)
     council = models.ForeignKey(DrvCouncil, on_delete=models.CASCADE)
     ato = models.ForeignKey(DrvAto, on_delete=models.CASCADE)
     street = models.ForeignKey(DrvStreet, on_delete=models.CASCADE)
     zip_code = models.ForeignKey(ZipCode, on_delete=models.CASCADE)
-    number = models.CharField(max_length=5)
-    
+    code = models.CharField(max_length=20, unique=True)
+    number = models.CharField(max_length=10)    
     def __str__(self):
         return self.number
